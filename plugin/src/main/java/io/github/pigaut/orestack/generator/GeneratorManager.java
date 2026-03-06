9b64db07ed0dfc53489c468046bcb06374d81dc0
Pigauts <sorrentinovirgilio@gmail.com>
2026-03-06 16:19:23 +0100

    version 3.43.0
    - Added restore-original-blocks-on-remove option to config.yml. Now if you place a generator that removes existing blocks, those blocks will be saved temporarly so that they can be replaced if you decide to remove the generator. keep-blocks-on-remove overrides this feature.
    
    - You can now place blocks against a generator reducing frustration when building your map. Must have the orestack.build.on.generator permission!
    
    - Completely reworked the generator state and logic and decoupled it from the generator class.
    
    - Fixed several issues and bugs related to version 1.16.5, if you find any other bugs I've missed please report them in the discord. For now this version should be stable.

--- plugin/src/main/java/io/github/pigaut/orestack/generator/GeneratorManager.java
+++ plugin/src/main/java/io/github/pigaut/orestack/generator/GeneratorManager.java
@@ -5,8 +5,6 @@ import io.github.pigaut.orestack.generator.template.*;
 import io.github.pigaut.orestack.util.*;
 import io.github.pigaut.sql.*;
 import io.github.pigaut.voxel.*;
-import io.github.pigaut.voxel.core.hologram.*;
-import io.github.pigaut.voxel.core.structure.*;
 import io.github.pigaut.voxel.plugin.manager.*;
 import io.github.pigaut.yaml.convert.parse.*;
 import io.github.pigaut.voxel.bukkit.Rotation;
@@ -22,6 +20,7 @@ public class GeneratorManager extends Manager {
     private final OrestackPlugin plugin;
     private final Set<Generator> generators = new HashSet<>();
     private final Map<Location, Generator> generatorBlocks = new ConcurrentHashMap<>();
+    private final Map<Generator, List<BlockState>> removedBlocks = new HashMap<>();
 
     public GeneratorManager(OrestackPlugin plugin) {
         super(plugin);
@@ -30,13 +29,16 @@ public class GeneratorManager extends Manager {
 
     @Override
     public void disable() {
-        for (Generator blockGenerator : generators) {
-            blockGenerator.cancelGrowth();
-            blockGenerator.removeBlocks();
-
-            HologramDisplay hologramDisplay = blockGenerator.getCurrentHologram();
-            if (hologramDisplay != null) {
-                hologramDisplay.destroy();
+        for (Generator generator : generators) {
+            GeneratorState state = generator.getState();
+            state.cancelGrowth();
+            state.removeBlocks();
+            state.removeHologram();
+            List<BlockState> removedBlocks = this.removedBlocks.remove(generator);
+            if (plugin.getSettings().isRestoreBlocksOnRemove() && removedBlocks != null) {
+                for (BlockState removedBlock : removedBlocks) {
+                    removedBlock.update(true, false);
+                }
             }
         }
     }
@@ -45,6 +47,7 @@ public class GeneratorManager extends Manager {
     public void loadData() {
         generators.clear();
         generatorBlocks.clear();
+        removedBlocks.clear();
 
         Database database = plugin.getDatabase();
         if (database == null) {
@@ -130,7 +133,7 @@ public class GeneratorManager extends Manager {
 
     @Override
     public void saveData() {
-        final Database database = plugin.getDatabase();
+        Database database = plugin.getDatabase();
         if (database == null) {
             logger.severe("Could not save data because database was not found.");
             return;
@@ -149,18 +152,18 @@ public class GeneratorManager extends Manager {
 
         database.clearTable("resources");
 
-        final DatabaseStatement insertStatement = database.merge("resources", "world, x, y, z",
+        DatabaseStatement insertStatement = database.merge("resources", "world, x, y, z",
                 "world", "x", "y", "z", "generator", "rotation", "stage");
 
         for (Generator generator : generators) {
-            final Location location = generator.getOrigin();
+            Location location = generator.getOrigin();
             insertStatement.withParameter(location.getWorld().getUID().toString());
             insertStatement.withParameter(location.getBlockX());
             insertStatement.withParameter(location.getBlockY());
             insertStatement.withParameter(location.getBlockZ());
             insertStatement.withParameter(generator.getTemplate().getName());
             insertStatement.withParameter(generator.getRotation().toString());
-            insertStatement.withParameter(generator.getCurrentStageId());
+            insertStatement.withParameter(generator.getState().getCurrentStage());
             insertStatement.addBatch();
         }
 
@@ -173,18 +176,18 @@ public class GeneratorManager extends Manager {
     }
 
     private void registerGenerator(String worldId, int x, int y, int z, String generatorName, String rotationData, int stage) throws GeneratorCreateException {
-        final World world = Bukkit.getWorld(UUID.fromString(worldId));
+        World world = Bukkit.getWorld(UUID.fromString(worldId));
         if (world == null) {
             throw new GeneratorCreateException(worldId, x, y, z, "world not found");
         }
 
-        final String worldName = world.getName();
-        final GeneratorTemplate template = plugin.getGeneratorTemplate(generatorName);
+        String worldName = world.getName();
+        GeneratorTemplate template = plugin.getGeneratorTemplate(generatorName);
         if (template == null) {
             throw new GeneratorCreateException(worldName, x, y, z, "generator template not found");
         }
 
-        final Location origin = new Location(world, x, y, z);
+        Location origin = new Location(world, x, y, z);
         Rotation rotation = ParseUtil.parseEnumOrNull(Rotation.class, rotationData);
         if (rotation == null) {
             rotation = Rotation.NONE;
@@ -192,7 +195,7 @@ public class GeneratorManager extends Manager {
                     worldName, x, y, z));
         }
 
-        final int maxStage = template.getMaxStage();
+        int maxStage = template.getMaxStage();
         if (stage > maxStage) {
             logger.warning(String.format("Failed to load stage of generator at %s, %d, %d, %d. Maximum stage (" + maxStage + ") has been applied.",
                     worldName, x, y, z));
@@ -204,8 +207,8 @@ public class GeneratorManager extends Manager {
             }
         }
 
-        final int finalStage = Math.min(stage, template.getMaxStage());
-        final Rotation finalRotation = rotation;
+        int finalStage = Math.min(stage, template.getMaxStage());
+        Rotation finalRotation = rotation;
         plugin.getScheduler().runTask(() -> {
             try {
                 Generator.create(template, origin, finalRotation, finalStage);
@@ -229,34 +232,37 @@ public class GeneratorManager extends Manager {
 
     public void registerGenerator(@NotNull Generator generator) throws GeneratorOverlapException {
         GeneratorTemplate template = generator.getTemplate();
+
+        List<BlockState> removedBlocks = new ArrayList<>();
         for (Block block : template.getAllOccupiedBlocks(generator.getOrigin(), generator.getRotation())) {
             if (plugin.getGenerators().isGenerator(block.getLocation())) {
                 throw new GeneratorOverlapException();
             }
+            removedBlocks.add(block.getState());
         }
 
         generators.add(generator);
         for (Block block : generator.getAllOccupiedBlocks()) {
             generatorBlocks.put(block.getLocation(), generator);
         }
+
+        if (plugin.getSettings().isRestoreBlocksOnRemove()) {
+            this.removedBlocks.put(generator, removedBlocks);
+        }
     }
 
     public void unregisterGenerator(@NotNull Generator generator) {
         generators.remove(generator);
+
         for (Block block : generator.getAllOccupiedBlocks()) {
             generatorBlocks.remove(block.getLocation());
         }
 
-        BlockStructure structure = generator.getCurrentStage().getStructure();
-        structure.remove(generator.getOrigin(), generator.getRotation());
-
-        HologramDisplay hologram = generator.getCurrentHologram();
-        if (hologram != null && hologram.exists()) {
-            hologram.destroy();
-        }
-
-        if (plugin.getSettings().isKeepBlocksOnRemove()) {
-            generator.getTemplate().getLastStage().getStructure().place(generator.getOrigin(), generator.getRotation());
+        List<BlockState> removedBlocks = this.removedBlocks.remove(generator);
+        if (plugin.getSettings().isRestoreBlocksOnRemove() && removedBlocks != null) {
+            for (BlockState removedBlock : removedBlocks) {
+                removedBlock.update(true, false);
+            }
         }
     }
 
